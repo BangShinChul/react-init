513e95f339dbc431cbf108d9a7ea0e83933252d1
Shinchul Bang <shinchul@finda.co.kr>
2020-01-08 19:41:56 +0900

    first commit

---
+++ src/components/button/DangerButton.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../../styles/Main.scss';
+
+class DangerButton extends Component {
+    render() {
+        return <Button color="red" />
+    }
+}
+
+export default DangerButton;
